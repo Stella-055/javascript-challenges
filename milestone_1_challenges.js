e1ec5b96d6c4339deeab326c229ff94494e591ea
Stella-055 <stellaongayo243@gmail.com>
2025-05-22 16:07:44 +0300

    removed comments and made code clean

--- milestone_1_challenges.js
+++ milestone_1_challenges.js
@@ -1,30 +1,30 @@
 //Challenge 1:Return the Sum of Two Numbers
 
-/*function addition(num1,num2) {
+function addition(num1,num2) {
    
     return num1+num2
 }
- addition(4,5)// 9
- */
+
+//console.log(addition(4,5))// 9
 
 //challenge 2: convert minutes to seconds
-/*function convert(minutes) {
+function convert(minutes) {
     return minutes*60
 }
-console.log(convert(5));// 300
-*/
+//console.log(convert(5));// 300
+
 
 //Challenge 3: Perimeter of a Rectangle
-/*function findPerimeter(length,width) {
+function findPerimeter(length,width) {
     let sum = length+width
     return 2*sum
 }
-console.log(findPerimeter(6,7));// 26
-*/
+//console.log(findPerimeter(6,7));// 26
+
 
 //Challenge 4:check negative
 
-/*function isNegative(number){
+function isNegative(number){
    if (number<0){
     return true;
 
@@ -32,12 +32,12 @@ console.log(findPerimeter(6,7));// 26
    else
    return false
 }
-console.log(isNegative(2))//false
-console.log(isNegative(-1))//true
-*/
+//console.log(isNegative(2))//false
+//console.log(isNegative(-1))//true
+
 
 //challenge 5:can drive
-/*function canDrive  (name,age){
+function canDrive  (name,age){
     if (age>=18){
         return name+ " is old enough to drive "
     }
@@ -45,11 +45,11 @@ console.log(isNegative(-1))//true
         return name+ " is not old enough to drive yet"
 };
 
-console.log(canDrive("stella",22))// stella  is old enough to drive
-*/
+//console.log(canDrive("stella",22))// stella  is old enough to drive
+
 
 //challenge 6:largest number
-/*function findLargest (a,b,c){
+function findLargest (a,b,c){
     if (a>b && a>b){
         return a
     }
@@ -62,13 +62,13 @@ console.log(canDrive("stella",22))// stella  is old enough to drive
     else
        return "they are equal or one is not a number "
  }
- console.log(findLargest(3,4,5))//5
- console.log(findLargest(5,5,99))//99
- */
+ //console.log(findLargest(3,4,5))//5
+ //console.log(findLargest(5,5,99))//99
+ 
 
 //challenge 7:BMI Calculator
 
-/*function calculateBMI(weight,height){
+function calculateBMI(weight,height){
    let BMI = weight/(height * height)
    if (BMI<18.5){
     return ` Your BMI is ${BMI}-under weight`
@@ -84,11 +84,11 @@ console.log(canDrive("stella",22))// stella  is old enough to drive
     else
       return ` Your BMI is ${BMI}-obese`
  }
- console.log(calculateBMI(68,1.75))//22.2
- */
+ //console.log(calculateBMI(68,1.75))//22.2
+ 
 
 //challenge 8: Greeting Based On Time
-/*function greetUser(name, hour){
+function greetUser(name, hour){
     if (hour>=5 && hour<=11){
         return `Good Morning, ${name}!`
     }
@@ -105,12 +105,12 @@ console.log(canDrive("stella",22))// stella  is old enough to drive
         return "Invalid hour"
 }
 
-console.log(greetUser("stella", 10))//Good Morning, stella!
-*/
+//console.log(greetUser("stella", 10))//Good Morning, stella!
+
 
 //challenge 9:  FizzBuzz
 
-/*function fizzBuzzCheck(number){
+function fizzBuzzCheck(number){
 
     if (number%3==0 && number%5==0){
         return "FizzBuzz"
@@ -125,12 +125,12 @@ console.log(greetUser("stella", 10))//Good Morning, stella!
         return number
 }
 
-console.log(fizzBuzzCheck(15))// FizzBuzz
-*/
+//console.log(fizzBuzzCheck(15))// FizzBuzz
+
 
 //challenge 10: Perimeter 2
 
-/*function  Perimeter(letterl, numbernum){
+function  Perimeter(letterl, numbernum){
  switch (letterl){
     case "s":
         return 4*numbernum;
@@ -142,12 +142,12 @@ console.log(fizzBuzzCheck(15))// FizzBuzz
         return "invalid shape"
  }
 }
-console.log(Perimeter("s",5))// 20
-*/
+//console.log(Perimeter("s",5))// 20
+
 
 //challenge 11:  Sum of Even Numbers
 
-/*function sumEvenNumbers(n){
+function sumEvenNumbers(n){
     let sum = 0;
     for (let i = 0; i <= n; i++) {
         if (i % 2 === 0) {
@@ -156,11 +156,11 @@ console.log(Perimeter("s",5))// 20
     }
     return sum;
 }
-console.log(sumEvenNumbers(6))//12 */
+//console.log(sumEvenNumbers(6))//12 
 
 //challenge 12:  Multiply by Itself
 
-/*function powerUp(num, times){
+function powerUp(num, times){
     let result=1
    
     for (let i = 0; i<times; i++) {
@@ -168,22 +168,22 @@ console.log(sumEvenNumbers(6))//12 */
     }
     return result;
 }
-console.log(powerUp(2,3))//8
-console.log(powerUp(5,0))//1 */
+//console.log(powerUp(2,3))//8
+//console.log(powerUp(5,0))//1 
 
 //challenge 13:Factorial Calculator
-/*function factorial(num){
+function factorial(num){
     let number=1
     for (i=1;i<=num;i++){
         number = number*i;
     }
        return number
 }
-console.log(factorial(4))//24*/
+//console.log(factorial(4))//24
 
 //challenge 14: Multiple Sum
 
-/*function sumMultiples(n, divisor){
+function sumMultiples(n, divisor){
     let sum=0
     for (let i = 1; i <= n; i++) {
         if (i % divisor === 0) {
@@ -193,11 +193,11 @@ console.log(factorial(4))//24*/
     return sum;
 }
  
-console.log(sumMultiples(10, 2))//30
-*/
+//console.log(sumMultiples(10, 2))//30
+
 
 //challenge 15:Sum of Digits
-/*function sumDigits(num) {
+function sumDigits(num) {
   let sum = 0;
   for ( let i = 0; num != 0; i++) {
     let digit1 = num % 10;
@@ -207,6 +207,6 @@ console.log(sumMultiples(10, 2))//30
   return sum;
 }
   
-console.log(sumDigits(123)); //6
-console.log(sumDigits(4567)); // 22
- */
+//console.log(sumDigits(123)); //6
+//console.log(sumDigits(4567)); // 22
+ 
